d73c520fb11e48ad1a9e50467e3e5dbc2b4b1bc8
Yaroslav Narozhnyi <gnredit9@gmail.com>
2026-01-19 14:07:19 +0200

    Data_Preparation_Yaroslav_N

---
+++ Data_Preparation/data_processing.py
@@ -0,0 +1,47 @@
+
+# **Importing all necessary libraries**
+
+import torch
+import torchvision
+import numpy as np
+import torch.nn as nn
+from sklearn.model_selection import train_test_split
+from torch.utils.data import Dataset, DataLoader
+import torchvision.transforms as transforms
+
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+
+
+train_tfms = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.RandomHorizontalFlip(),
+    transforms.RandomCrop(224, padding=8),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                         std=[0.229, 0.224, 0.225]),
+])
+
+test_tfms = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                         std=[0.229, 0.224, 0.225]),
+])
+train_full = torchvision.datasets.CIFAR10("./data", train=True, download=True, transform=train_tfms)
+test_set = torchvision.datasets.CIFAR10("./data", train=False, download=True, transform=test_tfms)
+
+
+val_size = 5000
+train_size = len(train_full) - val_size
+train_set, validation_set = torch.utils.data.random_split(
+    train_full,
+    [train_size, val_size],
+    generator=torch.Generator().manual_seed(42)
+)
+
+batch_size = 64
+
+train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=2)
+test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=False, num_workers=2)
+validation_loader = DataLoader(validation_set, batch_size=batch_size, shuffle=False, num_workers=2)
