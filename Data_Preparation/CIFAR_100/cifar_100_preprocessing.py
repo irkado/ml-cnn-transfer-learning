d0c1d05785e14bacaa0cda3091ead12b35f463b2
Yaroslav Narozhnyi <gnredit9@gmail.com>
2026-01-24 01:35:21 +0200

    CIFAR_100 Added

---
+++ Data_Preparation/CIFAR_100/cifar_100_preprocessing.py
@@ -0,0 +1,42 @@
+
+import torch
+import torchvision
+import numpy as np
+from torch.utils.data import DataLoader
+import torchvision.transforms as transforms
+
+from torchvision import datasets
+from torch.utils.data import DataLoader
+
+train_transforms = transforms.Compose([
+    transforms.Resize((224,224)),
+    transforms.RandomHorizontalFlip(),
+    transforms.RandomCrop(224,padding=8),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                         std=[0.229, 0.224, 0.225])
+])
+
+test_transforms = transforms.Compose([
+    transforms.Resize((224,224)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                         std=[0.229, 0.224, 0.225])
+])
+
+train_dataset = torchvision.datasets.CIFAR100("./data", train=True,transform=train_transforms,download=True)
+test_dataset = torchvision.datasets.CIFAR100("./data", train=False,transform=test_transforms,download=True)
+
+validation_size = int(len(train_dataset) * 0.1)
+
+train_set, validation_set =  torch.utils.data.random_split(train_dataset,lengths=[len(train_dataset) - validation_size, validation_size], generator=torch.Generator().manual_seed(42))
+
+print(f"The size of training set is {len(train_set)} samples")
+print(f"The size of validation set is {len(validation_set)} samples")
+
+batch_size = 64
+
+
+validation_loader = DataLoader(validation_set,batch_size=batch_size,shuffle=False,num_workers=2)
+train_loader = DataLoader(train_set, batch_size=batch_size,shuffle=True,num_workers=2, generator=torch.Generator().manual_seed(42))
+test_loader = DataLoader(test_dataset, batch_size=batch_size,shuffle=False)
