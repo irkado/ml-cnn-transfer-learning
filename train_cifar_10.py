f344787b672a7e55fca2e1e479ca85b218b39d5d
tikvi4ka <eknedeva@edu.aau.at>
2026-01-30 11:42:04 +0100

    commented out detirministic for the time being

--- train_cifar_10.py
+++ train_cifar_10.py
@@ -14,8 +14,10 @@ random.seed(SEED)
 np.random.seed(SEED)
 torch.manual_seed(SEED)
 torch.cuda.manual_seed(SEED)
-torch.backends.cudnn.deterministic = True
-torch.backends.cudnn.benchmark = False
+
+# for reproducibility
+# torch.backends.cudnn.deterministic = True
+# torch.backends.cudnn.benchmark = False
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print("Using device:", device)
